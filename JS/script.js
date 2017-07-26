45204ad49fc631a5acd1767bade984d258b987d4
Adam Weaver <parkersalterego92@gmail.com>
2017-07-26 11:25:34 -0700

    first-push

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+
+const FEATURED = document.querySelector(".featured-image");
+const THEIMAGE = FEATURED.querySelector("img");
+
+var altText = THEIMAGE.getAttribute("alt");
+
+captionElement.append(altText);
+FEATURED.append(captionElement);
+
+THEIMAGE.setAttribute("alt","");
